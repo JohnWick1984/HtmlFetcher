6a370e06f320d8b29c64c06ead52701b4d12fc6a
Евгений Шумаков <daredevil1984@list.ru>
2024-05-19 16:36:36 +0500

    Добавьте файлы проекта.

---
+++ HtmlFetcher/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HtmlFetcherLib
+{
+    public class HtmlFetcher
+    {
+        public async Task<string> GetHtmlAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    return await response.Content.ReadAsStringAsync();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Error fetching HTML", ex);
+                }
+            }
+        }
+    }
+}
